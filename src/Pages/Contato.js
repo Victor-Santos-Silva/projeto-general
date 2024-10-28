7bc2f1bc4e731a62e4eb52fcbdb6e376a9c480f9
Victor dos Santos da Silva <victorsantos.goal@gmail.com>
2024-10-28 16:35:52 -0300

    update em sobre e contato

--- src/Pages/Contato.js
+++ src/Pages/Contato.js
@@ -1,10 +1,68 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native'
 
 export default function Contato() {
+    const [nome, setNome] = useState('');
+    const [telefone, setTelefone] = useState('');
+   
+    const enviarContato = async () => {
+      if (!nome || !telefone) {
+        Alert.alert("Erro, por favor preencha todos os campos!");
+        return;
+      }
+   
+      const novoContato = { nome, telefone};
+   
+      axios.post('http://10.0.2.2:3000/contatos', novoContato)
+      .then(resposta => {
+        if (resposta.status === 201) {
+          Alert.alert("Sucesso, contato adicionado!");
+          setNome('');
+          setTelefone('');
+        } else {
+          Alert.alert("Erro, falha ao adicionar contato.");
+        }
+      })
+    }
+   
     return (
-        <View>
-            <Text>Contato</Text>
-        </View>
+      <View style={styles.container}>
+          <Text style={styles.label}>Nome</Text>
+          <TextInput
+          style={styles.input}
+          placeholder="Digite seu nome"
+          value={nome}
+          onChangeText={setNome}
+          />
+   
+          <Text style={styles.label} >Telefone</Text>
+          <TextInput
+          style={styles.input}
+          placeholder="Digite seu telefone"
+          value={telefone}
+          onChangeText={setTelefone}
+          />
+   
+          <Button title="Enviar contato" onPress={enviarContato}/>
+   
+      </View>
     )
-}
+  }
+   
+  const styles = StyleSheet.create({
+    container: {
+      padding: 20,
+    },
+    label: {
+      fontSize: 18,
+      marginBottom: 5
+    },
+    input: {
+      borderWidth: 1,
+      borderColor: "#ccc",
+      padding: 10,
+      marginBottom: 20,
+      borderRadius: 5
+    }
+  });
--- src/Pages/Sobre.js
+++ src/Pages/Sobre.js
@@ -1,60 +1,99 @@
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Alert, Button, ScrollView, StyleSheet, Text, View } from 'react-native'
 import axios from 'axios';
 
 export default function Sobre() {
     const [contatos, setContatos] = useState([])
 
-    // funcao para buscar contatos do servidor
+    // Funcao para buscar contatos do sevidor
     const listContatos = () => {
         axios
-            .get("http://10.0.2.2:3000/contatos")
+            .get('http://10.0.2.2:3000/contatos')
             .then((resposta) => {
                 setContatos(resposta.data)
             })
             .catch((error) => {
-                console.error("Erro ao buscar conatos.", error);
+                console.error("Erro ao buscar contatos", error)
+            })
+    }
+
+    //Função para excluir um contato
+    const deleteContato = (id) => {
+        axios
+            .delete(`http://10.0.2.2:3000/contatos/${id}`)
+            .then(() => {
+                //Atualizar a lista de contatos
+                setContatos(contatos.filter((contato) => contato.id !== id));
+                Alert.alert("Sucesso, contato excluido com sucesso");
+            })
+            .catch((error) => {
+                console.log("Erro ao excluir contato", error);
+                Alert.alert("Erro, não foi possivel excluir.")
+            })
+    }
+
+    //Função para editar um contato
+    const editarContato = (id) => {
+
+        axios
+            .put(`http://10.0.2.2:3000/contatos/${id}`)
+            .then(() => {
+                
+                Alert.alert("Sucesso, contato editado com sucesso");
+            })
+            .catch((error) => {
+                console.log("Erro ao editar contato", error);
+                Alert.alert("Erro, não foi possivel editar.")
             })
     }
 
     // Use o useEffect para buscar dados
     useEffect(() => {
         listContatos();
-    }, [])
+    }, []);
 
     return (
-        <View style={style.container}>
-            <Text style={style.title}>Lista de Contatos</Text>
-            {contatos.length > 0 ? (
-                contatos.map((contato, index) => (
-                    <View key={index} style={style.contatoItem}>
-                        <Text>{contato.nome}</Text>
-                        <Text>{contato.telefone}</Text>
-                    </View>
-                ))
-            ) : (
-                <Text style={style.noContacts}>Nenhum contato disponivel.</Text>
-            )
-            }
-        </View >
+        <ScrollView>
+            <View style={styles.container}>
+                <Text style={styles.title}>Lista de Contatos</Text>
+                {contatos.length > 0 ? (
+                    contatos.map((contato, index) => (
+                        <View key={index} style={styles.contatoItem}>
+                            <Text>{contato.nome}</Text>
+                            <Text>{contato.telefone}</Text>
+                            <Button
+                                title="Excluir"
+                                color="#4324d4"
+                                onPress={() => deleteContato(contato.id)}
+                            />
+                            <Button
+                                title="Editar"
+                                color="#4324d4"
+                                onPress={() => editarContato(contato.id)}
+                            />
+                        </View>
+                    ))
+                ) : (
+                    <Text style={styles.noContacts}>Nenhum contato disponivel</Text>
+                )}
+            </View>
+        </ScrollView>
     )
 }
 
-// Estilos
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
         padding: 10,
     },
     title: {
-        textAlign: "center",
         fontSize: 24,
         fontWeight: "bold",
-        marginBottom: 10
+        marginBottom: 10,
     },
     contatoItem: {
         padding: 10,
         borderBottomWidth: 1,
-        borderBottomColor: "#9d6ff0",
+        borderBottomColor: "#ccc"
     },
     noContacts: {
         fontSize: 16,
